7cd972d6b79f85063b26e930c24eb42399b95be5
shenghuayou <syou000@citymail.cuny.edu>
2017-02-22 22:08:22 -0500

    client script that connect to the server on my AWS

---
+++ chat_client_aws.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+ 
+# this client connects to the server that I host on aws, free to chat
+
+import socket
+from threading import Thread
+import sys
+ 
+# basic socket setup for client
+TCP_IP = '54.149.37.172'
+TCP_PORT = 6666
+BUFFER_SIZE = 1024
+username = input('enter a username: ')
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+
+# send username to server
+s.send(username.encode('utf-8'))
+
+# recv function keep running on thread
+def recv():
+    while 1:
+        data = s.recv(BUFFER_SIZE)
+        if data:
+        	print (data.decode('utf-8'))
+Thread(target=recv).start()
+
+# user will keep doing input
+while 1:
+	input_message = input('')
+	MESSAGE = ('%s: %s' % (username,input_message))
+	s.send(MESSAGE.encode('utf-8'))
+	
+s.close()
