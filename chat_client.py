085fc14d0b05fd604d2c71f18545d2697d08323f
shenghuayou <syou000@citymail.cuny.edu>
2017-02-22 22:11:14 -0500

    pass username to server

--- chat_client.py
+++ chat_client.py
@@ -4,13 +4,16 @@ import socket
 from threading import Thread
 import sys
  
- #basic socket setup for client
+# basic socket setup for client
 TCP_IP = 'localhost'
 TCP_PORT = 6666
 BUFFER_SIZE = 1024
-username = input('username:')
+username = input('enter a username: ')
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
+
+# send username to server
+s.send(username.encode('utf-8'))
 
 # recv function keep running on thread
 def recv():
@@ -18,7 +21,6 @@ def recv():
         data = s.recv(BUFFER_SIZE)
         if data:
         	print (data.decode('utf-8'))
-
 Thread(target=recv).start()
 
 # user will keep doing input
@@ -27,5 +29,4 @@ while 1:
 	MESSAGE = ('%s: %s' % (username,input_message))
 	s.send(MESSAGE.encode('utf-8'))
 	
-
 s.close()
--- chat_server.py
+++ chat_server.py
@@ -7,16 +7,17 @@ import pymysql
 import time
 
 def store_message(info):
-    #connect to database and perform query
-    db = pymysql.connect("<host>","<user>","<pass>","<db>")
+    # connect to database and store data
+    db = pymysql.connect("host","user","pass","db" )
     cursor = db.cursor()
     cursor.execute("insert into history(message) values (%s);", info)
     db.commit()
     db.close()
 
 def read_message():
+    # connect to database and retrieve data
     return_list=[]
-    db = pymysql.connect("<host>","<user>","<pass>","<db>")
+    db = pymysql.connect("host","user","pass","db" )
     cursor = db.cursor()
     cursor.execute("select * from history")
     result = cursor.fetchall()
@@ -37,7 +38,7 @@ def broadcast (server_socket, sock, message):
                     input.remove(socket)
     
 
-#basic socket setup for server
+# basic socket setup for server
 host = 'localhost' 
 backlog = 5 
 BUFFER_SIZE = 1024 
@@ -45,27 +46,37 @@ port = 6666
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 server.bind((host,port)) 
 server.listen(backlog) 
-input = [server,] #list of connection
+input = [server,] # list of connection
+
+# dict for client and username
+client_username_dict = {}
 
 print ('server is up, waiting for connections')
-running = 1 #set running to zero to close the server
-while running: 
+while 1: 
   inputready,outputready,exceptready = select.select(input,[],[]) 
 
   for s in inputready: #check each socket that select() said has available data
     if s == server: #if select returns our server socket, there is a new 
                     #remote socket trying to connect
       client, address = server.accept() 
-      input.append(client) #add it to the socket list so we can check it now
+      input.append(client) #add it to the socket list 
       print ('New client: %s'%str(address))
-      enter_data = "A client enter the room"
+
+      #decode username from client, and add it to dictionary
+      username_data = client.recv(BUFFER_SIZE)
+      decoded_username_data = username_data.decode('utf-8')
+      client_username_dict[client] = decoded_username_data
+
+      # tell connected client that a new client connects to server
+      enter_data ="---------------" + str(decoded_username_data) + " enter the room" + "---------------"
       encode_enter_data = enter_data.encode('utf-8')
       broadcast(server,client,encode_enter_data)
+
+      # send chat history to a new client
       history_list = read_message()
       for i in history_list:
         client.send(i.encode('utf-8'))
         time.sleep(0.01)
-      #broadcast(server,client,message_from_DB.encode('utf-8'))
 
     else: 
       # select has indicated that these sockets have data available to recv
@@ -74,9 +85,14 @@ while running:
         decode_data = str(data.decode('utf-8'))
         store_message(decode_data)
         broadcast(server, s, data)
-      else: 
-        left_data = "A client left the room"
+      else:
+        #send out message when a client left 
+        left_user = client_username_dict[s]
+        left_data = "---------------" + str(left_user) + " left the room" + "---------------" 
         broadcast(server,s,left_data.encode('utf-8'))
+
+        # remove this client from dictionary and socket list
+        del client_username_dict[s]
         s.close() 
         input.remove(s) 
 
